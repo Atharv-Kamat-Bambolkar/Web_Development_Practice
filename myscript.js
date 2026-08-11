10efadfd1f82b4dbb971c33c6294cf13a03d1491
Atharv-Kamat-Bambolkar <atharvkamatbambolkar2@gmail.com>
2026-08-11 13:42:41 +0530

    Eighteenth Commit

--- myscript.js
+++ myscript.js
@@ -1,155 +1,190 @@
-console.log("Hello World");
-console.log(5 + 6);
+// var x = 2;
 
-let x, y, z;
-x = 5;
-y = 6;
-z = x + y;
-console.log(z); 
+// function square(num) {
+//     var ans = num * num;
+//     return ans;
+// }
 
-// Assignment Operators - = += -= *= /= %=
-let a = 10;
-a += 5;
-console.log(a);
+// var square2 = square(x);
+// var square4 = square(4);
 
-let b = 10;
-b -= 5;
-console.log(b);
+console.log(x);
 
-let c = 10;
-c *= 5;
-console.log(c);
+let x = 2;
 
-let d = 10;
-d /= 5;
-console.log(d);
+// console.log("Hello World");
+// console.log(5 + 6);
 
-let e = 10;
-e %= 3;
-console.log(e);
+// let x, y, z;
+// x = 5;
+// y = 6;
+// z = x + y;
+// console.log(z); 
 
-// Logical Operators - && AND || OR ! NOT
-let a1 = true;
-let b1 = false;
-let c1 = a1 && b1;
-let d1 = a1 || b1;
-let e1 = !a1;
-console.log(c1);
-console.log(d1);
-console.log(e1);
+// // Assignment Operators - = += -= *= /= %=
+// let a = 10;
+// a += 5;
+// console.log(a);
 
-// Aithmetic Operators - + - * / % ** ++ --
-let a2 = 10;
-let b2 = 3;
-let c2 = a2 + b2;
-let d2 = a2 - b2;
-let e2 = a2 * b2;
-let f = a2 / b2;
-let g = a2 % b2;
-let h = a2 ** b2;
-let i = ++a2;
-let j = --b2;
-let k = +a2;
-let l = -b2;
-console.log(c2);
-console.log(d2);
-console.log(e2);
-console.log(f);
-console.log(g);
-console.log(h);
-console.log(i);
-console.log(j);
-console.log(k);
-console.log(l);
+// let b = 10;
+// b -= 5;
+// console.log(b);
 
-// Unary Plus Operator 
-let a3 = "10";
-let b3 = +a3;
-console.log(b);
-console.log(typeof a3);
-console.log(typeof b3);
+// let c = 10;
+// c *= 5;
+// console.log(c);
 
-let x1 = true;
-let y1 = false;
-console.log(+x1);
-console.log(+y1);
+// let d = 10;
+// d /= 5;
+// console.log(d);
 
-let p = "";
-console.log(+p);
+// let e = 10;
+// e %= 3;
+// console.log(e);
 
-let q = null;
-console.log(+q);
+// // Logical Operators - && AND || OR ! NOT
+// let a1 = true;
+// let b1 = false;
+// let c1 = a1 && b1;
+// let d1 = a1 || b1;
+// let e1 = !a1;
+// console.log(c1);
+// console.log(d1);
+// console.log(e1);
 
-let r = undefined;
-console.log(+r);
+// // Aithmetic Operators - + - * / % ** ++ --
+// let a2 = 10;
+// let b2 = 3;
+// let c2 = a2 + b2;
+// let d2 = a2 - b2;
+// let e2 = a2 * b2;
+// let f = a2 / b2;
+// let g = a2 % b2;
+// let h = a2 ** b2;
+// let i = ++a2;
+// let j = --b2;
+// let k = +a2;
+// let l = -b2;
+// console.log(c2);
+// console.log(d2);
+// console.log(e2);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+// console.log(l);
 
-// Unary Minus Operator
-let a4 = 10;
-let b4 = -a4;
-console.log(b2);
+// // Unary Plus Operator 
+// let a3 = "10";
+// let b3 = +a3;
+// console.log(b);
+// console.log(typeof a3);
+// console.log(typeof b3);
 
-let c3 = -20;
-let d3 = -c3;
-console.log(d3);
+// let x1 = true;
+// let y1 = false;
+// console.log(+x1);
+// console.log(+y1);
 
-let x2 = "25";
-let y2 = -x2;
-console.log(y2);
-console.log(typeof y2);
-console.log(typeof x2);
+// let p = "";
+// console.log(+p);
 
-let m = true;
-let n = false;
-console.log(-m);
-console.log(-n);
+// let q = null;
+// console.log(+q);
 
-// Comparison Operators
-let a5 = 10;
-let b5 = 20;
-let c4 = a == b;
-console.log(c4);
+// let r = undefined;
+// console.log(+r);
 
-let x3 = 10;
-let y3 = "10";
-let z1 = x3 == y3;
-console.log(z1);
+// // Unary Minus Operator
+// let a4 = 10;
+// let b4 = -a4;
+// console.log(b2);
 
-let p1 = 10;
-let q1 = "10";
-let r1 = p1 === q1;
-console.log(r1);
+// let c3 = -20;
+// let d3 = -c3;
+// console.log(d3);
 
-let m1 = 10;
-let n1 = 10;
-let o = m1 === n1;
-console.log(o);
+// let x2 = "25";
+// let y2 = -x2;
+// console.log(y2);
+// console.log(typeof y2);
+// console.log(typeof x2);
 
-let a6 = 10;
-let b6 = 20;
-let c5 = a6 != b6;
-console.log(c5);
+// let m = true;
+// let n = false;
+// console.log(-m);
+// console.log(-n);
 
-let x4 = 10;
-let y4 = "10";
-let z2 = x4 != y4;
-console.log(z2);
+// // Comparison Operators
+// let a5 = 10;
+// let b5 = 20;
+// let c4 = a == b;
+// console.log(c4);
 
-let p2 = 10;
-let q2 = 20;
-let r2 = p2 > q2;
-console.log(r2);
+// let x3 = 10;
+// let y3 = "10";
+// let z1 = x3 == y3;
+// console.log(z1);
 
-let m2 = 10;
-let n2 = 20;
-let o1 = m2 < n2;
-console.log(o1);
+// let p1 = 10;
+// let q1 = "10";
+// let r1 = p1 === q1;
+// console.log(r1);
 
-let a7 = 10;
-let b7 = 10;
-let c6 = a7 >= b7;
-console.log(c6);
+// let m1 = 10;
+// let n1 = 10;
+// let o = m1 === n1;
+// console.log(o);
 
-let x5 = 10;
-let y5 = 20;
-let z3 = x5 <= y5;
-console.log(z3);
+// let a6 = 10;
+// let b6 = 20;
+// let c5 = a6 != b6;
+// console.log(c5);
+
+// let x4 = 10;
+// let y4 = "10";
+// let z2 = x4 != y4;
+// console.log(z2);
+
+// let p2 = 10;
+// let q2 = 20;
+// let r2 = p2 > q2;
+// console.log(r2);
+
+// let m2 = 10;
+// let n2 = 20;
+// let o1 = m2 < n2;
+// console.log(o1);
+
+// let a7 = 10;
+// let b7 = 10;
+// let c6 = a7 >= b7;
+// console.log(c6);
+
+// let x5 = 10;
+// let y5 = 20;
+// let z3 = x5 <= y5;
+// console.log(z3);
+
+// var x = 10;
+
+// if (true) {
+//     const x = 20;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+// if (true) {
+//     var x = 20;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+// if (true) {
+//     let x = 20;
+//     console.log(x);
+// }
